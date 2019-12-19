e1ff1797d97510ad91cbd5248201ec7a1dc70cd0
VectoriaR <wangziru19@126.com>
2019-12-19 23:33:24 +0800

    产品页面的结构第一次建立

--- js/index.js
+++ js/index.js
@@ -6,15 +6,3 @@ $('#logo ul>li').on('mouseenter',function(){
 $('#logo ul>li').on('mouseleave',function(){
     $('#logo .subMenu').hide();
 })
-// 轮播
-$("#demo3").slideBox({
-    duration: 0.3, //滚动持续时间，单位：秒
-
-    easing: "linear", //swing,linear//滚动特效
-
-    delay: 5, //滚动延迟时间，单位：秒
-
-    hideClickBar: false, //不自动隐藏点选按键
-
-    clickBarRadius: 10
-  });
